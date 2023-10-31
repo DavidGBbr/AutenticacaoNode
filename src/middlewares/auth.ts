61db7d6d64b15e219f21889069a11f890f534d62
davidGBbr <davidgomesbrigido@gmail.com>
2023-10-31 11:56:15 -0300

    Fazendo autenticação Basic Auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,10 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import { User } from "../models/User";
 
 export const Auth = {
-  private: (req: Request, res: Response, next: NextFunction) => {
+  private: async (req: Request, res: Response, next: NextFunction) => {
     let success = false;
 
-    //Fazer processo de verificação
+    if (req.headers.authorization) {
+      let hash: string = req.headers.authorization.substring(6);
+      let decoded: string = Buffer.from(hash, "base64").toString();
+      let data: string[] = decoded.split(":");
+
+      if (data.length === 2) {
+        let hasUser = await User.findOne({
+          where: {
+            email: data[0],
+            password: data[1],
+          },
+        });
+        if (hasUser) {
+          success = true;
+        }
+      }
+    }
 
     if (success) {
       next();
